fa4c454ecacde689bca45b1836f3c31bb0e38775
Sergio Flores Ledezma (zerch) <zerchito@gmail.com>
2023-05-18 23:08:55 -0400

    Solution (easy): Nth fibonacci

---
+++ src/nth-fibonacci.ts
@@ -0,0 +1,11 @@
+export function getNthFib(n: number) {
+  let lastTwo = [0,1];
+  let counter = 3;
+  while (counter <= n) {
+    let nextFib = lastTwo[0] + lastTwo[1];
+    lastTwo[0] = lastTwo[1];
+    lastTwo[1] = nextFib;
+    counter +=1;
+  }
+  return n > 1 ? lastTwo[1] : 0;
+}
