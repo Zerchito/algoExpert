d141c4e28473ce8d4a53d115ed78e9c613b09773
Sergio Flores Ledezma (zerch) <zerchito@gmail.com>
2023-05-28 22:03:28 -0400

    solution (easy): Middle node

---
+++ src/middleNode.ts
@@ -0,0 +1,20 @@
+export class LinkedList {
+  value: number;
+  next: LinkedList | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export function middleNode(linkedList: LinkedList) {
+  let slowNode:LinkedList | null = linkedList;
+  let fastNode:LinkedList | null = linkedList;
+  while (fastNode && fastNode.next) {
+    slowNode = slowNode?.next ?? null;
+    fastNode = fastNode.next.next;
+  }
+  
+  return slowNode;
+}
